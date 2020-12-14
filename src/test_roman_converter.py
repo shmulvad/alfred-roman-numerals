526ad18c5a4ae6075149283ad9f531db6d1c1b5f
Søren Hougaard Mulvad <shmulvad@gmail.com>
2020-12-14 19:16:53 +0800

    Use classes in test for better readability

--- src/test_roman_converter.py
+++ src/test_roman_converter.py
@@ -2,91 +2,77 @@ import pytest
 from roman_converter import roman_to_int, int_to_roman, convert
 
 
-# roman_to_int
-def test_roman_to_int_two_symbol():
-    assert roman_to_int('IV') == '4'
+class TestRomanToInt:
+    def test_two_symbol(self):
+        assert roman_to_int('IV') == '4'
+
+    def test_hard(self):
+        assert roman_to_int('MMDLXXVII') == '2577'
+
+    def test_max(self):
+        assert roman_to_int('MMMCMXCIX') == '3999'
+
+    def test_gibberish_raises_error(self):
+        with pytest.raises(ValueError):
+            roman_to_int('k')
 
 
-def test_roman_to_int_hard():
-    assert roman_to_int('MMDLXXVII') == '2577'
+class TestIntToRoman:
+    def test_simple(self):
+        assert int_to_roman(1) == 'I'
+
+    def test_two_symbol(self):
+        assert int_to_roman(4) == 'IV'
+
+    def test_hard(self):
+        assert int_to_roman(2577) == 'MMDLXXVII'
+
+    def test_max(self):
+        assert int_to_roman(3999) == 'MMMCMXCIX'
 
 
-def test_roman_to_int_max():
-    assert roman_to_int('MMMCMXCIX') == '3999'
+class TestConvert:
+    def test_num_less(self):
+        result, _, is_valid = convert('0')
+        assert result == "Can't convert 0 and negative numbers"
+        assert is_valid is False
 
+    def test_num_min(self):
+        result, _, is_valid = convert('1')
+        assert result == 'I'
+        assert is_valid
 
-def test_roman_to_int_gibberish_raises_error():
-    with pytest.raises(ValueError):
-        roman_to_int('k')
+    def test_num_max(self):
+        result, _, is_valid = convert('3999')
+        assert result == 'MMMCMXCIX'
+        assert is_valid
 
+    def test_num_greater(self):
+        result, _, is_valid = convert('4000')
+        assert result == 'Numbers greater than 3999 are not represented'
+        assert is_valid is False
 
-# int_to_roman
-def test_int_to_roman_simple():
-    assert int_to_roman(1) == 'I'
+    def test_roman_min(self):
+        result, _, is_valid = convert('I')
+        assert result == '1'
+        assert is_valid
 
+    def test_roman_max(self):
+        result, _, is_valid = convert('MMMCMXCIX')
+        assert result == '3999'
+        assert is_valid
 
-def test_int_to_roman_two_symbol():
-    assert int_to_roman(4) == 'IV'
+    def test_roman_small_letters(self):
+        result, _, is_valid = convert('iii')
+        assert result == '3'
+        assert is_valid
 
+    def test_roman_invalid(self):
+        result, _, is_valid = convert('IC')
+        assert result == 'Not a valid roman number'
+        assert is_valid is False
 
-def test_int_to_roman_hard():
-    assert int_to_roman(2577) == 'MMDLXXVII'
-
-
-def test_int_to_roman_max():
-    assert int_to_roman(3999) == 'MMMCMXCIX'
-
-
-# convert
-def test_convert_num_less():
-    result, _, is_valid = convert('0')
-    assert result == "Can't convert 0 and negative numbers"
-    assert is_valid is False
-
-
-def test_convert_num_min():
-    result, _, is_valid = convert('1')
-    assert result == 'I'
-    assert is_valid
-
-
-def test_convert_num_max():
-    result, _, is_valid = convert('3999')
-    assert result == 'MMMCMXCIX'
-    assert is_valid
-
-
-def test_convert_num_greater():
-    result, _, is_valid = convert('4000')
-    assert result == 'Numbers greater than 3999 are not represented'
-    assert is_valid is False
-
-
-def test_convert_roman_min():
-    result, _, is_valid = convert('I')
-    assert result == '1'
-    assert is_valid
-
-
-def test_convert_roman_max():
-    result, _, is_valid = convert('MMMCMXCIX')
-    assert result == '3999'
-    assert is_valid
-
-
-def test_convert_roman_small_letters():
-    result, _, is_valid = convert('iii')
-    assert result == '3'
-    assert is_valid
-
-
-def test_convert_roman_invalid():
-    result, _, is_valid = convert('IC')
-    assert result == 'Not a valid roman number'
-    assert is_valid is False
-
-
-def test_convert_gibberish():
-    result, _, is_valid = convert('k')
-    assert result == 'Cannot evaluate expression'
-    assert is_valid is False
+    def test_gibberish(self):
+        result, _, is_valid = convert('k')
+        assert result == 'Cannot evaluate expression'
+        assert is_valid is False
